35758767502d9aeb1c9ee31680adecd90ce18ec3
Flavion <flaviondsa@yahoo.in>
2019-05-03 12:52:14 +0530

    Added pdf

--- javascript/home.js
+++ javascript/home.js
@@ -41,7 +41,7 @@ material.home.ready = function() {
             $('#pw').val('');
             dialog.open();
           } else {
-            window.location.href = '/pdf/confirmation.pdf';
+            window.location.href = '/pdf/third-sunday-easter.pdf';
           }
         }
       })
