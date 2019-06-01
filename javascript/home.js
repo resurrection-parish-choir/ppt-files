315283213fb3f8d1512643b874fba97aa712dbfc
Flavion <flaviondsa@yahoo.in>
2019-06-01 19:45:28 +0530

    Added pdf

--- javascript/home.js
+++ javascript/home.js
@@ -27,7 +27,7 @@ material.home.ready = function() {
       $('.download.mdc-button')
         .on('click', event => {
           //dialog.open();
-          window.location.href = '/pdf/sixth-sunday-easter.pdf';
+          window.location.href = '/pdf/seventh-sunday-easter.pdf';
         })
       ;
       
