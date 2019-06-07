29d60319da9d51283a056b0e3f17675a9d50c655
Flavion <flaviondsa@yahoo.in>
2019-06-07 13:13:59 +0530

    Added pdf

--- javascript/home.js
+++ javascript/home.js
@@ -27,7 +27,7 @@ material.home.ready = function() {
       $('.download.mdc-button')
         .on('click', event => {
           //dialog.open();
-          window.location.href = '/pdf/seventh-sunday-easter.pdf';
+          window.location.href = '/pdf/pentecost-sunday.pdf';
         })
       ;
       
