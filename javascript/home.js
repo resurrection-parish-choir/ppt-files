daef12218ffec8d1f9cca250ea633611747f31d3
Flavion <flaviondsa@yahoo.in>
2019-07-26 14:32:02 +0530

    Added pdf

--- javascript/home.js
+++ javascript/home.js
@@ -27,7 +27,7 @@ material.home.ready = function() {
       $('.download.mdc-button')
         .on('click', event => {
           //dialog.open();
-          window.location.href = '/pdf/sixteenth-sunday-ot.pdf';
+          window.location.href = '/pdf/seventeenth-sunday-ot.pdf';
         })
       ;
       
