e217600ef356b0b8e20b27d91e7cb469957a147b
Flavion <flaviondsa@yahoo.in>
2019-08-24 21:29:46 +0530

    Added pdf

--- javascript/home.js
+++ javascript/home.js
@@ -27,7 +27,7 @@ material.home.ready = function() {
       $('.download.mdc-button')
         .on('click', event => {
           //dialog.open();
-          window.location.href = '/pdf/twentieth-sunday-ot.pdf';
+          window.location.href = '/pdf/twenty-first-sunday-ot.pdf';
         })
       ;
       
