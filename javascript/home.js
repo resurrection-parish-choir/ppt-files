ec553a91d15d1b2eaef1a2070f2077e4abe90b0c
Flavion <flaviondsa@yahoo.in>
2019-11-03 11:10:43 +0530

    Added pdf

--- javascript/home.js
+++ javascript/home.js
@@ -27,7 +27,7 @@ material.home.ready = function() {
       $('.download.mdc-button')
         .on('click', event => {
           //dialog.open();
-          window.location.href = '/pdf/thirtieth-sunday-ot.pdf';
+          window.location.href = '/pdf/thirty-first-sunday-ot.pdf';
         })
       ;
       
